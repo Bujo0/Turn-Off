9a84143f02c158cd9ed8443ab34f2ce5894670a6
Filip Bujaroski <filipbujaroski@gmail.com>
2017-04-12 20:20:50 -0400

    Homepage shows total amount lost

--- add_money.js
+++ add_money.js
@@ -15,7 +15,8 @@ function show_money() {
     frozen = Math.round(JSON.parse(localStorage.current_user).amount_frozen)
     money.empty().append("<a href='#add_money' class='ui-btn ui-icon-lock ui-btn-icon-left' data-transition='flow'>" +
                     "<h3> Amount Frozen:</h3> <p>$" + frozen + " </p>" +
-                    "<h3> Daily Refund: </h3> <p>$" + daily + " </p>"  
+                    "<h3> Daily Amount: </h3> <p>$" + daily + " </p>"  +
+                    "<h3> Total Amount Lost: </h3> <p>$" + JSON.parse(localStorage.current_user).amount_lost + " </p>" 
                     );
             
 }
