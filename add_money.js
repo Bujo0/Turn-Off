75a16195f17af359c8ebe67e3c620b161bab3de5
Jiacheng Zhao <jiachengzhao@Jiachengs-MacBook-Pro.local>
2017-04-12 21:48:15 -0400

    update guardian done

--- add_money.js
+++ add_money.js
@@ -23,17 +23,23 @@ function show_money() {
 
 function show_update_guardian() {
 
+    // var guardian_list_html = '';
+    // for (i in JSON.parse(localStorage.current_user).guardian_lst) {
+    //     guardian_list_html += ('<label for="guardian" >'  + JSON.parse(localStorage.current_user).guardian_lst[i] + '</label>' + '<br> '+
+    //     '<input type="radio"  name = "guardian_n" value="' + JSON.parse(localStorage.current_user).guardian_lst[i] + '"> ' )
+    // }
+
+    // console.log(guardian_list_html)
+
+
     update_guardian_form.empty().append('<form action="/action_page.php">' +
           'Goal: ' + JSON.parse(localStorage.current_user).smoking_target + ' cigeratte(s)<br>' + 
           'Cigarette Count Today:<br>' +
           '<input type="text" id="today_count"><br><br>' +
-        '<fieldset data-role="controlgroup">' + 
-            '<legend>Select Guardian to Notify</legend>' +
-              '<label for="jason">Jason</label>' + 
-              '<input type="checkbox"  id="jason" value="Jason">' + 
-              '<label for="yoav">Yoav</label>' + 
-              '<input type="checkbox"  id="yoav" value="Yoav">' + 
-        '</fieldset>' + 
+        // '<fieldset data-role="controlgroup">' + 
+        //     '<legend>Select Guardian to Notify</legend>' +
+        //     guardian_list_html + 
+        // '</fieldset>' + 
         '<button data-rel="back" data-icon="ui-btn-icon-left ui-icon-back" class="ui-btn" onclick="daily_check_in();">Check In</button>' + 
         '</form>');
             
@@ -100,82 +106,107 @@ function daily_check_in() {
 
     var today_count = document.getElementById("today_count").value;
 
-    // I shall probably make guardian names into variables
-    var guardian_name = ""
-
-
     smoking_history = JSON.parse(localStorage.current_user).smoking_history;
 
+    if( !(isNaN(parseFloat(today_count)))) {
+        if ((isFloat(parseFloat(today_count))) || (isInteger(parseFloat(today_count)))) {
+            var timeDiff = Math.abs(JSON.parse(localStorage.current_user).due_date - Date.now())
+            var days_left = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+            daily = JSON.parse(localStorage.current_user).amount_frozen / days_left
 
+            var temp_user = JSON.parse(localStorage.current_user)
+            var entry = {count: parseFloat(today_count), date: Date(Date.now()), target: temp_user.smoking_target, money: daily}
+            smoking_history.push(entry)
+            temp_user.smoking_history = smoking_history
+            if(temp_user.smoking_target < today_count){
+                temp_user.amount_frozen -= daily
+                temp_user.amount_lost += daily
+            }
+            else {
+                temp_user.amount_won += daily
+            }
 
+            localStorage.current_user = JSON.stringify(temp_user)
+            show_target()
 
+            alert_text = "You smoked " + today_count + " cigeratte(s) today. " + "A message of confirmation has been sent to " + JSON.parse(localStorage.current_user).guardian+ "."
+            alert(alert_text)
 
+            window.location.reload();
+            window.location.replace("index.html#landing")
+
+        }
+        else {
+            $.mobile.pageLoadErrorMessage = "sorry you must input a number";
+            window.location.replace("index.html#update_guardian")
+        }   
+    }
 
 
     
-    if ((document.getElementById("jason").checked) || (document.getElementById("yoav").checked)) {
-        if ((document.getElementById("jason").checked) && (document.getElementById("yoav").checked)) {
-            alert("you can only select 1 guardian")
-            $.mobile.pageLoadErrorMessage = "";
-            window.location.replace("index.html#update_guardian")
-        }
-        else {
-            if (document.getElementById("jason").checked) {
-                guardian_name = document.getElementById("jason").value
-            }
-            if (document.getElementById("yoav").checked) {
-                guardian_name = document.getElementById("yoav").value
-            }
-            if( !(isNaN(parseFloat(today_count)))) {
-                if ((isFloat(parseFloat(today_count))) || (isInteger(parseFloat(today_count)))) {
-                    var timeDiff = Math.abs(JSON.parse(localStorage.current_user).due_date - Date.now())
-                    var days_left = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-                    daily = JSON.parse(localStorage.current_user).amount_frozen / days_left
+    // if ((document.getElementById("jason").checked) || (document.getElementById("yoav").checked)) {
+    //     if ((document.getElementById("jason").checked) && (document.getElementById("yoav").checked)) {
+    //         alert("you can only select 1 guardian")
+    //         $.mobile.pageLoadErrorMessage = "";
+    //         window.location.replace("index.html#update_guardian")
+    //     }
+    //     else {
+    //         if (document.getElementById("jason").checked) {
+    //             guardian_name = document.getElementById("jason").value
+    //         }
+    //         if (document.getElementById("yoav").checked) {
+    //             guardian_name = document.getElementById("yoav").value
+    //         }
+    //         if( !(isNaN(parseFloat(today_count)))) {
+    //             if ((isFloat(parseFloat(today_count))) || (isInteger(parseFloat(today_count)))) {
+    //                 var timeDiff = Math.abs(JSON.parse(localStorage.current_user).due_date - Date.now())
+    //                 var days_left = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+    //                 daily = JSON.parse(localStorage.current_user).amount_frozen / days_left
 
-                    var temp_user = JSON.parse(localStorage.current_user)
-                    var entry = {count: parseFloat(today_count), date: Date(Date.now()), target: temp_user.smoking_target, money: daily}
-                    smoking_history.push(entry)
-                    temp_user.smoking_history = smoking_history
-                    if(temp_user.smoking_target < today_count){
-                        temp_user.amount_frozen -= daily
-                        temp_user.amount_lost += daily
-                    }
-                    else {
-                        temp_user.amount_won += daily
-                    }
+    //                 var temp_user = JSON.parse(localStorage.current_user)
+    //                 var entry = {count: parseFloat(today_count), date: Date(Date.now()), target: temp_user.smoking_target, money: daily}
+    //                 smoking_history.push(entry)
+    //                 temp_user.smoking_history = smoking_history
+    //                 if(temp_user.smoking_target < today_count){
+    //                     temp_user.amount_frozen -= daily
+    //                     temp_user.amount_lost += daily
+    //                 }
+    //                 else {
+    //                     temp_user.amount_won += daily
+    //                 }
 
-                    localStorage.current_user = JSON.stringify(temp_user)
-                    show_target()
+    //                 localStorage.current_user = JSON.stringify(temp_user)
+    //                 show_target()
 
-                    alert_text = "You smoked " + today_count + " cigeratte(s) today. " + "A message of confirmation has been sent to " + guardian_name+ "."
-                    alert(alert_text)
+    //                 alert_text = "You smoked " + today_count + " cigeratte(s) today. " + "A message of confirmation has been sent to " + guardian_name+ "."
+    //                 alert(alert_text)
 
-                    window.location.reload();
-                    window.location.replace("index.html#landing")
+    //                 window.location.reload();
+    //                 window.location.replace("index.html#landing")
 
-                }
-                else {
-                    $.mobile.pageLoadErrorMessage = "sorry you must input a number";
-                    window.location.replace("index.html#update_guardian")
-                }   
-            }
-            else {
-                $.mobile.pageLoadErrorMessage = "sorry you must fill in the form";
-                window.location.replace("index.html#update_guardian")
+    //             }
+    //             else {
+    //                 $.mobile.pageLoadErrorMessage = "sorry you must input a number";
+    //                 window.location.replace("index.html#update_guardian")
+    //             }   
+    //         }
+    //         else {
+    //             $.mobile.pageLoadErrorMessage = "sorry you must fill in the form";
+    //             window.location.replace("index.html#update_guardian")
 
-            }
+    //         }
 
-        }
+    //     }
 
 
         
 
 
-    }
-    else {
-         alert("You need to select a guardian")
+    // }
+    // else {
+    //      alert("You need to select a guardian")
 
-    }
+    // }
 
 
     
